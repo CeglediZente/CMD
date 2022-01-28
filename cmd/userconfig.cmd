99146c43e6621d4417b64ba0db0e3f41829742a1
Ceglédi Zente Holló <nadizente@gmail.com>
2022-01-28 22:04:37 +0100

    User Config

---
+++ cmd/userconfig.cmd
@@ -0,0 +1,8 @@
+net user /add <név> <jelszó>
+net user <név> /active yes
+net user <név> /expires: never
+net user <név> /fullname -
+net user <név> /passwordchg: no
+net user <név> /passwordreq: yes
+net user <név> /times Hétfő-Csütörtök,04:00-22:00;Péntek-Szombat,04:00-23:00;Vasárnap,04:00-22:00
+
